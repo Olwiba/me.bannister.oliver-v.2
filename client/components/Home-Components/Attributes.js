dab623df466cb5b6cd570ecae6ed8ca971e3186b
olwiba <olwiba@gmail.com>
2016-08-31 14:11:01 +1200

    Added in attributes component

---
+++ client/components/Home-Components/Attributes.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Attributes extends Component {
+
+  render() {
+    return (
+      <section id='section-attributes'>	
+        <div className='attributes'>
+          <div className='center'>
+            <h1 className='black'>Attr section</h1>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Attributes
--- client/components/Home.js
+++ client/components/Home.js
@@ -4,6 +4,7 @@ import Navigation from './Home-Components/Navigation'
 import Splash from './Home-Components/Splash'
 import Intro from './Home-Components/Intro'
 import About from './Home-Components/About'
+import Attributes from './Home-Components/Attributes'
 import Skills from './Home-Components/Skills'
 import History from './Home-Components/History'
 import Projects from './Home-Components/Projects'
@@ -27,6 +28,7 @@ class Home extends Component {
         <Splash/>
         <Intro/>
         <About/>
+        <Attributes/>
         <Skills/>
         <History/>
         <Projects/>
